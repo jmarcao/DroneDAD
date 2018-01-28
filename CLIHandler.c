11ad044936d0c10ea2fcb0d2c3a43318b2db7f99
riddhindoshi <31804490+riddhindoshi@users.noreply.github.com>
2018-01-28 18:05:16 -0500

    individual files

---
+++ CLIHandler.c
@@ -0,0 +1,384 @@
+/*
+ * CLIHandler.c
+ *
+ * Created: 1/24/2018 7:34:10 PM
+ *  Author: John
+ */ 
+
+#include "asf.h"
+
+#include <string.h>
+
+#include "CLIHandler.h"
+#include "Version.h"
+#include "adc_temp.h"
+
+const char* CMD_HELP = "help";
+const char* CMD_VER_BL = "ver_bl";
+const char* CMD_VER_APP = "ver_app";
+const char* CMD_GPIO_SET = "gpio_set";
+const char* CMD_GPIO_CLEAR = "gpio_clear";
+const char* CMD_GPIO_GET = "gpio_get";
+const char* CMD_MAC = "mac";
+const char* CMD_IP = "ip";
+const char* CMD_READ_GYRO = "read_gyro";
+const char* CMD_READ_ACCEL = "read_accel";
+const char* CMD_ADC_GET = "adc_get";
+const char* CMD_MCU_TEMP = "mcu_temp";
+const char* CMD_I2C_SCAN = "i2c_scan";
+
+extern struct adc_module adc_inst;
+extern struct i2c_master_module i2c_master_instance;
+
+void init_cmd_list() {
+	cmd_list = (struct option_list) {
+		{{ CMD_HELP, "", "Display all commands" } ,
+		{ CMD_VER_BL, "", "Print bootloader firmware information." } ,
+		{ CMD_VER_APP, "", "Print application version information." } ,
+		{ CMD_GPIO_SET, "[port] [pin_num]", "Set GPIO pin at a given port high." } ,
+		{ CMD_GPIO_CLEAR, "[port] [pin_num]", "Set GPIO pin at a given port low." } ,
+		{ CMD_GPIO_GET, "[port] [pin_num]", "Get state of a GPIO pin.(Enter B 2 or B 3)" } ,
+		{ CMD_MAC, "", "Print the MAC address." } ,
+		{ CMD_IP, "", "Print the IP Address." } ,
+		{ CMD_READ_GYRO, "[reading count] [interval in ms]", "Read from the Gyroscope sensor." } ,
+		{ CMD_READ_ACCEL, "[reading count] [interval in ms]", "Read from the Accelerometer sensor." } ,
+		{ CMD_ADC_GET, "[port] [pin_num]", "Get the ADC value of an input pin." } ,
+		{ CMD_MCU_TEMP, "", "Print the temperature reading of the on-board MCU temperature sensor." } ,
+		{ CMD_I2C_SCAN, "", "Print out list of addresses of I2C devices on bus." }}
+	};
+}
+
+static void disable_adc(void) {
+	adc_disable(&adc_inst);
+}
+
+static void configure_adc(void) {
+	struct adc_config config;
+	
+	adc_get_config_defaults(&config);
+	config.clock_source = GCLK_GENERATOR_1;
+	config.reference = ADC_REFERENCE_INTVCC1;
+	config.clock_prescaler = ADC_CTRLB_PRESCALER_DIV16;
+	config.resolution = ADC_RESOLUTION_12BIT;
+	adc_init(&adc_inst, ADC, &config);
+	adc_enable(&adc_inst);
+}
+
+// TODO: If arg1 or arg2 are non-digit, we need to throw an error.
+void handle_user_input(char* input) {
+
+	char* cmd = strtok(input, " ");
+
+
+	
+	if(strcmp(CMD_HELP, cmd) == 0) { 
+		handle_help(); 
+	}
+	else if(strcmp(CMD_VER_BL, cmd) == 0) { 
+		handle_ver_bl(); 
+	}
+	else if(strcmp(CMD_VER_APP, cmd) == 0) { 
+		handle_ver_app();
+	}
+	else if(strcmp(CMD_GPIO_SET, cmd) == 0) { 
+		char* arg1 = strtok(NULL, " ");
+		char* arg2 = strtok(NULL, " ");
+		
+		if(arg1 == 0 || arg2 == 0) {
+			printf("Not enough arguments! Try \"help\".\r\n");
+			return;
+		}
+
+		char* port = arg1;
+		int pin_num = atoi(arg2);
+
+		if ((strcmp(port,"A") != 0) && (strcmp(port,"B") != 0)){
+			printf("Enter a valid Port! Try \"help\".\r\n");
+		}
+
+		if (pin_num <= 0 && pin_num > 32) {
+			printf("Enter a valid Pin Number! Try \"help\".\r\n");
+		}
+		
+		handle_gpio_set(*port, pin_num);
+	}
+	else if(strcmp(CMD_GPIO_CLEAR, cmd) == 0) { 
+		char* arg1 = strtok(NULL, " ");
+		char* arg2 = strtok(NULL, " ");
+		
+		if(arg1 == 0 || arg2 == 0) {
+			printf("Not enough arguments! Try \"help\".\r\n");
+			return;
+		}
+
+		char* port = arg1;
+		int pin_num = atoi(arg2);
+
+		if ((strcmp(port,"A") != 0) && (strcmp(port,"B") != 0)){
+			printf("Enter a valid Port! Try \"help\".\r\n");
+		}
+
+		if (pin_num <= 0 && pin_num > 32) {
+			printf("Enter a valid Pin Number! Try \"help\".\r\n");
+		}
+		
+		handle_gpio_clear(*port, pin_num); 
+	}
+	else if(strcmp(CMD_GPIO_GET, cmd) == 0) {
+		char* arg1 = strtok(NULL, " ");
+		char* arg2 = strtok(NULL, " ");
+		
+		if(arg1 == 0 || arg2 == 0) {
+			printf("Not enough arguments! Try \"help\".\r\n");
+			return;
+		}
+
+		char* port = arg1;
+		int pin_num = atoi(arg2);
+
+		if ((strcmp(port,"A") != 0) && (strcmp(port,"B") != 0)){
+			printf("Enter a valid Port! Try \"help\".\r\n");
+		}
+
+		if (pin_num <= 0 && pin_num > 32) {
+			printf("Enter a valid Pin Number! Try \"help\".\r\n");
+		}
+
+		handle_gpio_get(*port, pin_num);
+	}
+	else if(strcmp(CMD_MAC, cmd) == 0) { 
+		handle_mac(); 
+	}
+	else if(strcmp(CMD_IP, cmd) == 0) { 
+		handle_ip(); 
+	}
+	else if(strcmp(CMD_READ_GYRO, cmd) == 0) {
+		char* arg1 = strtok(NULL, " ");
+		char* arg2 = strtok(NULL, " ");
+		
+		if(arg1 == 0 || arg2 == 0) {
+			printf("Not enough arguments! Try \"help\".\r\n");
+			return;
+		}
+				
+		int num_readings = atoi(arg1);
+		int interval_ms = atoi(arg2);
+		
+		handle_read_gyro(num_readings, interval_ms); 
+	}
+	else if(strcmp(CMD_READ_ACCEL, cmd) == 0) {
+		char* arg1 = strtok(NULL, " ");
+		char* arg2 = strtok(NULL, " ");
+		
+		if(arg1 == 0 || arg2 == 0) {
+			printf("Not enough arguments! Try \"help\".\r\n");
+			return;
+		}
+				
+		int num_readings = atoi(arg1);
+		int interval_ms = atoi(arg2);
+		
+		handle_read_accel(num_readings, interval_ms); 
+	}
+	else if(strcmp(CMD_ADC_GET, cmd) == 0) {
+		char* arg1 = strtok(NULL, " ");
+		char* arg2 = strtok(NULL, " ");
+		
+		if(arg1 == 0 || arg2 == 0) {
+			printf("Not enough arguments! Try \"help\".\r\n");
+			return;
+		}
+
+		char* port = arg1;
+		int pin_num = atoi(arg2);
+
+		if ((strcmp(port,"A") != 0) && (strcmp(port,"B") != 0)){
+			printf("Enter a valid Port! Try \"help\".\r\n");
+		}
+
+		if (pin_num <= 0 && pin_num > 32) {
+			printf("Enter a valid Pin Number! Try \"help\".\r\n");
+		}
+		
+		handle_adc_get(port, pin_num); 
+	}
+	else if(strcmp(CMD_MCU_TEMP, cmd) == 0) { 
+		handle_mcu_temp(); 
+	}
+	else if(strcmp(CMD_I2C_SCAN, cmd) == 0) { 
+		handle_i2c_scan(); 
+	}
+	else {
+		printf("Invalid command! Type \"help\" for a list of available commands.\r\n");
+	}
+}
+
+void handle_help() {
+	printf("Command List:\r\n");
+	for(int i = 0; i < OPTION_COUNT; i++) {
+		printf("\t%s %s\r\n\t\t%s\r\n", cmd_list.options[i].command, 
+			cmd_list.options[i].params,
+			cmd_list.options[i].description);
+	}
+}
+
+
+void handle_ver_bl() {
+	printf("Bootloader Version: %s\r\n", BL_VERSION_STRING);
+}
+
+void handle_ver_app() {
+	printf("Application Version: %s\r\n", APP_VERSION_STRING);
+}
+
+void handle_gpio_set(char port, int pin_num) {
+	// TODO: Must implement this feature for at least two pins.
+	
+	// Hard-coded testing
+	uint8_t pin = PIN_PB02;
+	bool level = HIGH;
+	port_pin_set_output_level(pin, level);
+	
+	printf("Not implemented yet!\r\n");
+}
+
+void handle_gpio_clear(char port, int pin_num) {
+	// TODO: Must implement this feature for at least two pins.
+	
+	// Hard-coded testing
+	uint8_t pin = PIN_PB02;
+	bool level = LOW;
+	port_pin_set_output_level(pin, level);
+		
+	printf("Not implemented yet!\r\n");
+}
+
+void handle_gpio_get(char port, int pin_num) {
+	// TODO: Must implement this feature for at least two pins.
+		uint8_t pin = PIN_PB02;
+		bool level;
+
+	// Hard-coded testing
+	if (port == "B" && pin_num == 2){
+		level = port_pin_get_output_level(pin);
+	}
+
+	else if (port == "B" && pin_num == 3){
+		uint8_t pin = PIN_PB03;
+		level = port_pin_get_output_level(pin);
+	}
+	
+	printf("The level set is %d\r\n", level);
+
+}
+
+void handle_mac() {
+	char* dummy_mac = "01:23:45:67:89:ab";
+	printf("MAC Address: %s\r\n", dummy_mac);
+}
+
+void handle_ip() {
+	char* dummy_ip = "255.255.255.255";
+	printf("IP Address: %s\r\n", dummy_ip);
+}
+
+void handle_read_gyro(int num_readings, int interval_ms) {
+	printf("Reading from Gyro...\r\n");
+	// Dummy data
+	for(int i = 0; i < num_readings; i++) {
+		printf("%d\r\n", 9);
+		delay_ms(interval_ms);
+	}
+	printf("Done!\r\n");
+}
+
+void handle_read_accel(int num_readings, int interval_ms) {
+	printf("Reading from Accelerometer...\r\n");
+	// Dummy data
+	for(int i = 0; i < num_readings; i++) {
+		printf("%d\r\n", 8);
+		delay_ms(interval_ms);
+	}
+	printf("Done!\r\n");
+}
+
+void handle_adc_get(char port, int pin_num) {
+	disable_adc();
+	configure_adc();
+	
+	adc_start_conversion(&adc_inst);
+	uint16_t result;
+	
+	do {
+		/* Wait */
+	} while(adc_read(&adc_inst, &result) == STATUS_BUSY);
+	
+	printf("ADC Result: %d\r\n", result);
+}
+
+void handle_mcu_temp() {
+	disable_adc();
+	
+	system_voltage_reference_enable(SYSTEM_VOLTAGE_REFERENCE_TEMPSENSE);
+	
+	configure_adc_temp();
+	
+	adc_start_conversion(&adc_inst);
+	uint16_t result;
+	
+	do {
+		/* Wait */
+	} while(adc_read(&adc_inst, &result) == STATUS_BUSY);
+	
+	int temp = calculate_temperature(result);
+	
+	printf("Temperature Result: %d \r\n", temp);
+}
+
+// TODO: Put this in a cleaner place
+#define DATA_LENGTH 8
+static uint8_t wr_buffer[DATA_LENGTH] = {
+	0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07
+};
+static uint8_t rd_buffer[DATA_LENGTH];
+
+#define SLAVE_ADDRESS (0x52 >> 1)
+/// 0x52 0x53 0x29
+
+struct i2c_master_module i2c_master_instance;
+
+static struct i2c_master_packet wr_packet = {
+	.address          = SLAVE_ADDRESS,
+	.data_length      = DATA_LENGTH,
+	.data             = wr_buffer,
+	.ten_bit_address  = false,
+	.high_speed       = false,
+	.hs_master_code   = 0x00,
+};
+
+static struct i2c_master_packet rd_packet = {
+	.address          = SLAVE_ADDRESS,
+	.data_length      = DATA_LENGTH,
+	.data             = rd_buffer,
+	.ten_bit_address  = false,
+	.high_speed       = false,
+	.hs_master_code   = 0x00,
+};
+
+void handle_i2c_scan() {
+	
+	  for(char slave_address = 0; slave_address <= 127; slave_address++) {
+		  enum status_code i2c_status;
+		  wr_packet.address     = slave_address;
+		  wr_packet.data_length = 1;
+		  wr_buffer[0]          = 0x00;
+		  wr_packet.data        = wr_buffer;
+		  i2c_status = i2c_master_write_packet_wait_no_stop(&i2c_master_instance, &wr_packet);
+		  if( i2c_status == STATUS_OK ) {
+			  //i2c_status = i2c_master_read_packet_wait(&i2c_master_instance, &rd_packet);
+			 printf("The slave address is %d \r\n", slave_address); 
+		  }
+		  i2c_master_send_stop(&i2c_master_instance);
+	 }
+	
+}
--- adc_temp.h
+++ adc_temp.h
@@ -1,9 +1,9 @@
-﻿/**
+/**
  * \file
  *
- * \brief ADC Temperature Sensor configuration file
+ * \brief ADC temperature sensor macro definition and prototype.
  *
- * Copyright (C) 2015 Atmel Corporation. All rights reserved.
+ * Copyright (c) 2015 Atmel Corporation. All rights reserved.
  *
  * \asf_license_start
  *
@@ -40,166 +40,26 @@
  * \asf_license_stop
  *
  */
- /**
- * Support and FAQ: visit <a href="http://www.atmel.com/design-support/">Atmel Support</a>
- */
-#include <asf.h>
-#include "adc_temp.h"
 
-/* Structure for ADC module instance */
-extern struct adc_module adc_instance;
+#ifndef ADC_TEMP_H_INCLUDED
+#define ADC_TEMP_H_INCLUDED
+
+/* TEMP_LOG Base Address*/
+#define TEMP_LOG_READ_NVM1					(*(RwReg*)NVMCTRL_TEMP_LOG)     
+/* TEMP_LOG Next Address*/
+#define TEMP_LOG_READ_NVM2					(*(RwReg*)NVMCTRL_TEMP_LOG + 4) 
+#define ADC_TEMP_SAMPLE_LENGTH				4
+#define INT1V_VALUE_FLOAT					1.0
+#define INT1V_DIVIDER_1000					1000.0
+#define ADC_12BIT_FULL_SCALE_VALUE_FLOAT	4095.0
 
 
-/*  The following variables have referred with respect to device data sheet 
-	Equation1 and Equation1b on section "Temperature Sensor Characteristics" 
-	of Electrical Characteristics */
 
-float coarse_temp; /* Coarse value of the temperature - tempC */ 
-float fine_temp;   /* Finer value of the temperature - tempF */
 
-float tempR;       /* Production Room Temperature value read from NVM memory - tempR */
-float tempH;	   /* Production Hot Temperature value read from NVM memory - tempH */
-float INT1VR;      /* Room temp 2�s complement of the internal 1V reference value - INT1VR */
-float INT1VH;	   /* Hot temp 2�s complement of the internal 1V reference value - INT1VR */
-uint16_t ADCR;     /* Production Room Temperature ADC Value read from NVM memory - ADCR */
-uint16_t ADCH;     /* Production Hot Temperature ADC Value read from NVM memory - ADCH */
+/* Function Prototypes for the ADC Temp Sensor Operation */
+void configure_adc_temp(void);
 
-float VADCR;	   /* Room Temperature ADC voltage - VADCR */
-float VADCH;	   /* Hot Temperature ADC voltage - VADCH */
+int calculate_temperature(int adc_result);
 
-/**
-* \brief ADC Temperature Sensor mode configuration.
-* This function enables internal temperature sensor feature of ADC with below Settings
 
-* GLCK for ADC		-> GCLK_GENERATOR_1 (8MHz)
-* CLK_ADC			-> 512 KHz
-* REFERENCE			-> internal 1 V
-* POSITIVE INPUT	-> INTRENAL Temperature reference
-* NEGATIVE INPUT	-> 
-* SAMPLES			-> 4
-* SAMPLE_LENGTH		-> 4
-*/
-
-void configure_adc_temp(void)
-{
-	struct adc_config conf_adc;
-	
-	adc_get_config_defaults(&conf_adc);
-	
-	conf_adc.clock_source = GCLK_GENERATOR_1;
-	conf_adc.clock_prescaler = ADC_CLOCK_PRESCALER_DIV16;
-	conf_adc.reference = ADC_REFERENCE_INT1V;
-	conf_adc.positive_input = ADC_POSITIVE_INPUT_TEMP;
-	conf_adc.negative_input = ADC_NEGATIVE_INPUT_GND;
-	conf_adc.sample_length = ADC_TEMP_SAMPLE_LENGTH;
-	
-	adc_init(&adc_instance, ADC, &conf_adc);
-	
-	ADC->AVGCTRL.reg = ADC_AVGCTRL_ADJRES(2) | ADC_AVGCTRL_SAMPLENUM_4;
-	
-	adc_enable(&adc_instance);
-}
-
-/**
-* \brief Decimal to Fraction Conversation.
-* This function converts the decimal value into fractional 
-* and return the fractional value for temperature calculation
-*/
-float convert_dec_to_frac(uint8_t val)
-{
-	if (val < 10)
-	{
-		return ((float)val/10.0);
-	}
-	
-	else if (val <100)
-	{
-		return ((float)val/100.0);
-	}
-	
-	else
-	{
-		return ((float)val/1000.0);
-	}
-}
-
-/**
-* \brief Calibration Data.
-* This function extract the production calibration data information from
-* Temperature log row content and store it variables for temperature calculation
-*
-*/
-void load_calibration_data(void)
-{
-	volatile uint32_t val1;				/* Temperature Log Row Content first 32 bits */
-	volatile uint32_t val2;				/* Temperature Log Row Content another 32 bits */
-	uint8_t room_temp_val_int;			/* Integer part of room temperature in �C */
-	uint8_t room_temp_val_dec;			/* Decimal part of room temperature in �C */
-	uint8_t hot_temp_val_int;			/* Integer part of hot temperature in �C */
-	uint8_t hot_temp_val_dec;			/* Decimal part of hot temperature in �C */
-	int8_t room_int1v_val;				/* internal 1V reference drift at room temperature */
-	int8_t hot_int1v_val;				/* internal 1V reference drift at hot temperature*/
-	
-	uint32_t *temp_log_row_ptr = (uint32_t *)NVMCTRL_TEMP_LOG;
-	
-	val1 = *temp_log_row_ptr;
-	temp_log_row_ptr++;
-	val2 = *temp_log_row_ptr;
-	
-	room_temp_val_int = (uint8_t)((val1 & NVMCTRL_FUSES_ROOM_TEMP_VAL_INT_Msk) >> NVMCTRL_FUSES_ROOM_TEMP_VAL_INT_Pos);
-	
-	room_temp_val_dec = (uint8_t)((val1 & NVMCTRL_FUSES_ROOM_TEMP_VAL_DEC_Msk) >> NVMCTRL_FUSES_ROOM_TEMP_VAL_DEC_Pos);
-	
-	hot_temp_val_int = (uint8_t)((val1 & NVMCTRL_FUSES_HOT_TEMP_VAL_INT_Msk) >> NVMCTRL_FUSES_HOT_TEMP_VAL_INT_Pos);
-	
-	hot_temp_val_dec = (uint8_t)((val1 & NVMCTRL_FUSES_HOT_TEMP_VAL_DEC_Msk) >> NVMCTRL_FUSES_HOT_TEMP_VAL_DEC_Pos);
-	
-	room_int1v_val = (int8_t)((val1 & NVMCTRL_FUSES_ROOM_INT1V_VAL_Msk) >> NVMCTRL_FUSES_ROOM_INT1V_VAL_Pos);
-	
-	hot_int1v_val = (int8_t)((val2 & NVMCTRL_FUSES_HOT_INT1V_VAL_Msk) >> NVMCTRL_FUSES_HOT_INT1V_VAL_Pos);
-	
-	ADCR = (uint16_t)((val2 & NVMCTRL_FUSES_ROOM_ADC_VAL_Msk) >> NVMCTRL_FUSES_ROOM_ADC_VAL_Pos);
-	
-	ADCH = (uint16_t)((val2 & NVMCTRL_FUSES_HOT_ADC_VAL_Msk) >> NVMCTRL_FUSES_HOT_ADC_VAL_Pos);
-	
-	tempR = room_temp_val_int + convert_dec_to_frac(room_temp_val_dec);
-	
-	tempH = hot_temp_val_int + convert_dec_to_frac(hot_temp_val_dec);
-	
-	INT1VR = 1 - ((float)room_int1v_val/INT1V_DIVIDER_1000);
-	
-	INT1VH = 1 - ((float)hot_int1v_val/INT1V_DIVIDER_1000);
-	
-	VADCR = ((float)ADCR * INT1VR)/ADC_12BIT_FULL_SCALE_VALUE_FLOAT;
-	
-	VADCH = ((float)ADCH * INT1VH)/ADC_12BIT_FULL_SCALE_VALUE_FLOAT;
-}
-
-/**
-* \brief Temperature Calculation.
-* This function calculate fine temperature using Equation1 and Equation
-* 1b as mentioned in data sheet section "Temperature Sensor Characteristics"
-* of Electrical Characteristics.
-*
-*/
-float calculate_temperature(uint16_t raw_code)
-{
-	float VADC;      /* Voltage calculation using ADC result for Coarse Temp calculation */
-	float VADCM;     /* Voltage calculation using ADC result for Fine Temp calculation. */
-	float INT1VM;    /* Voltage calculation for reality INT1V value during the ADC conversion */
-	
-	VADC = ((float)raw_code * INT1V_VALUE_FLOAT)/ADC_12BIT_FULL_SCALE_VALUE_FLOAT;
-	
-	/* Coarse Temp Calculation by assume INT1V=1V for this ADC conversion */
-	coarse_temp = tempR + (((tempH - tempR)/(VADCH - VADCR)) * (VADC - VADCR));
-	
-	/* Calculation to find the real INT1V value during the ADC conversion */
-	INT1VM = INT1VR + (((INT1VH - INT1VR) * (coarse_temp - tempR))/(tempH - tempR));
-	
-	VADCM = ((float)raw_code * INT1VM)/ADC_12BIT_FULL_SCALE_VALUE_FLOAT;
-	
-	/* Fine Temp Calculation by replace INT1V=1V by INT1V = INT1Vm for ADC conversion */
-	fine_temp = tempR + (((tempH - tempR)/(VADCH - VADCR)) * (VADCM - VADCR));
-	
-	return fine_temp;
-}
+#endif /* ADC_TEMP_H_INCLUDED */
