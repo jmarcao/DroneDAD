bd1fe7a8d19653701a58b1ec2791a5aedcb3965b
jmarcao <john.marcao@gmail.com>
2018-01-29 21:37:13 -0500

    Added Sensor and Actuator handling

--- DroneDadProj/src/CLIHandler.c
+++ DroneDadProj/src/CLIHandler.c
@@ -23,6 +23,8 @@ const char* CMD_MAC = "mac";
 const char* CMD_IP = "ip";
 const char* CMD_READ_GYRO = "read_gyro";
 const char* CMD_READ_ACCEL = "read_accel";
+const char* CMD_SET_ACT = "set_act";
+const char* CMD_CLEAR_ACT = "clear_act";
 const char* CMD_ADC_GET = "adc_get";
 const char* CMD_MCU_TEMP = "mcu_temp";
 const char* CMD_I2C_SCAN = "i2c_scan";
@@ -41,12 +43,46 @@ void init_cmd_list() {
 		{ CMD_MAC, "", "Print the MAC address." } ,
 		{ CMD_IP, "", "Print the IP Address." } ,
 		{ CMD_READ_GYRO, "[reading count] [interval in ms]", "Read from the Gyroscope sensor." } ,
-		{ CMD_READ_ACCEL, "[reading count] [interval in ms]", "Read from the Accelerometer sensor." } ,
+		//{ CMD_READ_ACCEL, "[reading count] [interval in ms]", "Read from the Accelerometer sensor." } ,
+		{ CMD_SET_ACT, "", "Turn the actuator (LED) on." } ,
+		{ CMD_CLEAR_ACT, "", "Turn the actuator (LED) off." } ,
 		{ CMD_ADC_GET, "[port] [pin_num]", "Get the ADC value of an input pin." } ,
 		{ CMD_MCU_TEMP, "", "Print the temperature reading of the on-board MCU temperature sensor." } ,
 		{ CMD_I2C_SCAN, "", "Print out list of addresses of I2C devices on bus." }}
 	};
 }
+
+// TODO: Put this in a cleaner place
+#define DATA_LENGTH 8
+static uint8_t wr_buffer[DATA_LENGTH] = {
+	0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07
+};
+
+#define READ_DATA_LENGTH 14
+static uint8_t rd_buffer[DATA_LENGTH];
+
+#define SLAVE_ADDRESS (0x52 >> 1)
+/// 0x52 0x53 0x29
+
+#define MPU_6050_SLAVE_ADDR (0x68)
+
+static struct i2c_master_packet wr_packet = {
+	.address          = SLAVE_ADDRESS,
+	.data_length      = DATA_LENGTH,
+	.data             = wr_buffer,
+	.ten_bit_address  = false,
+	.high_speed       = false,
+	.hs_master_code   = 0x00,
+};
+
+static struct i2c_master_packet rd_packet = {
+	.address          = SLAVE_ADDRESS,
+	.data_length      = READ_DATA_LENGTH,
+	.data             = rd_buffer,
+	.ten_bit_address  = false,
+	.high_speed       = false,
+	.hs_master_code   = 0x00,
+};
 
 static void disable_adc(void) {
 	adc_disable(&adc_inst);
@@ -66,11 +102,8 @@ static void configure_adc(void) {
 
 // TODO: If arg1 or arg2 are non-digit, we need to throw an error.
 void handle_user_input(char* input) {
-
 	char* cmd = strtok(input, " ");
 
-
-	
 	if(strcmp(CMD_HELP, cmd) == 0) { 
 		handle_help(); 
 	}
@@ -208,6 +241,14 @@ void handle_user_input(char* input) {
 	else if(strcmp(CMD_I2C_SCAN, cmd) == 0) { 
 		handle_i2c_scan(); 
 	}
+	else if(strcmp(CMD_SET_ACT, cmd) == 0) {
+		// Just a GPIO.
+		handle_gpio_set('B', PIN_PB02);	
+	}
+	else if(strcmp(CMD_CLEAR_ACT, cmd) == 0) {
+		// Just a GPIO.		
+		handle_gpio_clear('B', PIN_PB02);	
+	}
 	else {
 		printf("Invalid command! Type \"help\" for a list of available commands.\r\n");
 	}
@@ -239,7 +280,7 @@ void handle_gpio_set(char port, int pin_num) {
 	bool level = HIGH;
 	port_pin_set_output_level(pin, level);
 	
-	printf("Not implemented yet!\r\n");
+	//printf("Not implemented yet!\r\n");
 }
 
 void handle_gpio_clear(char port, int pin_num) {
@@ -250,7 +291,7 @@ void handle_gpio_clear(char port, int pin_num) {
 	bool level = LOW;
 	port_pin_set_output_level(pin, level);
 		
-	printf("Not implemented yet!\r\n");
+	//printf("Not implemented yet!\r\n");
 }
 
 void handle_gpio_get(char port, int pin_num) {
@@ -283,23 +324,73 @@ void handle_ip() {
 }
 
 void handle_read_gyro(int num_readings, int interval_ms) {
-	printf("Reading from Gyro...\r\n");
-	// Dummy data
+	enum status_code i2c_status;
+	
 	for(int i = 0; i < num_readings; i++) {
-		printf("%d\r\n", 9);
+		// Wake up the device
+		wr_packet.address     = MPU_6050_SLAVE_ADDR;
+		wr_packet.data_length = 1;
+		wr_buffer[0]          = 0x6B;
+		wr_packet.data        = wr_buffer;
+		rd_packet.address = MPU_6050_SLAVE_ADDR;
+		i2c_status = i2c_master_write_packet_wait_no_stop(&i2c_master_instance, &wr_packet);
+		if( i2c_status == STATUS_OK ) {
+			i2c_status = i2c_master_read_packet_wait(&i2c_master_instance, &rd_packet);
+		}
+		i2c_master_send_stop(&i2c_master_instance);
+	
+		rd_packet.data[0] = rd_packet.data[0] & (0 << 6);
+	
+		// Write new sleep bit (off)
+		wr_packet.address     = MPU_6050_SLAVE_ADDR;
+		wr_packet.data_length = 2;
+		wr_buffer[0]          = 0x6B;
+		wr_buffer[1]          = rd_packet.data[0];
+		wr_packet.data        = wr_buffer;
+		rd_packet.address = MPU_6050_SLAVE_ADDR;
+		i2c_status = i2c_master_write_packet_wait_no_stop(&i2c_master_instance, &wr_packet);
+		i2c_master_send_stop(&i2c_master_instance);
+	
+		// Read back the Power management Register 
+		wr_packet.address     = MPU_6050_SLAVE_ADDR;
+		wr_packet.data_length = 1;
+		wr_buffer[0]          = 0x6B;
+		wr_packet.data        = wr_buffer;
+		rd_packet.address = MPU_6050_SLAVE_ADDR;
+		i2c_status = i2c_master_write_packet_wait_no_stop(&i2c_master_instance, &wr_packet);
+		if( i2c_status == STATUS_OK ) {
+			i2c_status = i2c_master_read_packet_wait(&i2c_master_instance, &rd_packet);
+		}
+		i2c_master_send_stop(&i2c_master_instance);
+		
+		// Get the data.
+		wr_packet.address     = MPU_6050_SLAVE_ADDR;
+		wr_packet.data_length = 1;
+		wr_buffer[0]          = 0x3B; //MPU6050_RA_ACCEL_XOUT_H;
+		wr_packet.data        = wr_buffer;
+		rd_packet.address = MPU_6050_SLAVE_ADDR;
+		i2c_status = i2c_master_write_packet_wait_no_stop(&i2c_master_instance, &wr_packet);
+		if( i2c_status == STATUS_OK ) {
+			i2c_status = i2c_master_read_packet_wait(&i2c_master_instance, &rd_packet);
+		}
+	
+		int16_t ax, ay, az, gx, gy, gz;
+		ax = (((int16_t)rd_packet.data[0]) << 8) | rd_packet.data[1];
+		ay = (((int16_t)rd_packet.data[2]) << 8) | rd_packet.data[3];
+		az = (((int16_t)rd_packet.data[4]) << 8) | rd_packet.data[5];
+		gx = (((int16_t)rd_packet.data[8]) << 8) | rd_packet.data[9];
+		gy = (((int16_t)rd_packet.data[10]) << 8) | rd_packet.data[11];
+		gz = (((int16_t)rd_packet.data[12]) << 8) | rd_packet.data[13];
+	
+		printf("===Reading %d===\r\n", i);
+		printf("Ax = %d\r\nAy = %d\r\nAz = %d\r\nGx = %d\r\nGy = %d\r\nGz = %d\r\n", ax, ay, az, gx, gy, gz);
+		i2c_master_send_stop(&i2c_master_instance);
 		delay_ms(interval_ms);
 	}
-	printf("Done!\r\n");
 }
 
 void handle_read_accel(int num_readings, int interval_ms) {
-	printf("Reading from Accelerometer...\r\n");
-	// Dummy data
-	for(int i = 0; i < num_readings; i++) {
-		printf("%d\r\n", 8);
-		delay_ms(interval_ms);
-	}
-	printf("Done!\r\n");
+	printf("Use read_gyro!\r\n");
 }
 
 void handle_adc_get(char port, int pin_num) {
@@ -335,36 +426,6 @@ void handle_mcu_temp() {
 	printf("Temperature Result: %d \r\n", temp);
 }
 
-// TODO: Put this in a cleaner place
-#define DATA_LENGTH 8
-static uint8_t wr_buffer[DATA_LENGTH] = {
-	0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07
-};
-static uint8_t rd_buffer[DATA_LENGTH];
-
-#define SLAVE_ADDRESS (0x52 >> 1)
-/// 0x52 0x53 0x29
-
-struct i2c_master_module i2c_master_instance;
-
-static struct i2c_master_packet wr_packet = {
-	.address          = SLAVE_ADDRESS,
-	.data_length      = DATA_LENGTH,
-	.data             = wr_buffer,
-	.ten_bit_address  = false,
-	.high_speed       = false,
-	.hs_master_code   = 0x00,
-};
-
-static struct i2c_master_packet rd_packet = {
-	.address          = SLAVE_ADDRESS,
-	.data_length      = DATA_LENGTH,
-	.data             = rd_buffer,
-	.ten_bit_address  = false,
-	.high_speed       = false,
-	.hs_master_code   = 0x00,
-};
-
 void handle_i2c_scan() {
 	
 	  for(char slave_address = 0; slave_address <= 127; slave_address++) {
@@ -376,7 +437,7 @@ void handle_i2c_scan() {
 		  i2c_status = i2c_master_write_packet_wait_no_stop(&i2c_master_instance, &wr_packet);
 		  if( i2c_status == STATUS_OK ) {
 			  //i2c_status = i2c_master_read_packet_wait(&i2c_master_instance, &rd_packet);
-			 printf("The slave address is %d \r\n", slave_address); 
+			 printf("The slave address is %.2x \r\n", slave_address);
 		  }
 		  i2c_master_send_stop(&i2c_master_instance);
 	 }
